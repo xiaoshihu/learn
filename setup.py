0a1b1c8f5b90a2ac8a944b4c5be3f566a9a25bfd
xiaoshihu <38485245+xiaoshihu@users.noreply.github.com>
2019-04-21 20:17:49 +0800

    setup

---
+++ setup.py
@@ -0,0 +1,41 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="YXtest-pkg-xiaoshihu",
+    version="1.0.3",
+    author="xiaoshihu",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    packages=setuptools.find_packages(),
+    # 打包非python文件
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: windows",
+    ],
+    # 指定依赖库
+    install_requires=[
+        'SQLAlchemy',
+        'pandas',
+        'lxml',
+        'pywin32',
+        'concurrent-log-handler',
+        'openpyxl',
+        'chardet',
+        'xlrd',
+        'opencv-contrib-python',
+        'paramiko',
+        'yagmail',
+        'urllib3',
+        'Pillow',
+    ],
+    # 指定下载库的连接
+    dependency_links=['https://pypi.tuna.tsinghua.edu.cn/simple'],
+)
